e0be69ae58d4fed238a0a7266ce40216effc2f16
Robmel12 <rob.melnyk@gmail.com>
2022-11-03 18:57:03 -0600

    Created meme generator with updated css

--- react-tut/src/App.js
+++ react-tut/src/App.js
@@ -3,14 +3,19 @@ import Header from './components/Header';
 import './App.css';
 import Meme  from './components/Meme';
 import MemeStyles  from './components/Meme.module.scss'
+import Things from './components/Things';
+import Messages from './components/Messages';
+import { useState } from 'react';
 
 function App() {
-  
+  const [data, setData] = useState({});
+  fetch("https://source.unsplash.com/collection/928423/480x480").then(res => res.json()).then(data =>console.log(data)).catch(err => console.log(err))
   return (
     <>
    <Header/>
-   <Meme/>
-
+   <Meme/> 
+   {/* <Things/>
+   {/* <Messages/> */}
    </>
   );
 }
--- react-tut/src/components/Meme.js
+++ react-tut/src/components/Meme.js
@@ -1,30 +1,52 @@
-import React, { useState } from 'react'
-import MemeStyles  from './Meme.module.scss'
+import React, { useState } from "react";
+import MemeStyles from "./Meme.module.scss";
 
-
-
-export default function Meme(){
+export default function Meme() {
   const [memeWords, setMemeWords] = useState({
-    firstWords: '',
-    secondWords: ''
-  })
-    function handleClick(e){
-        e.preventDefault();
-        setMemeWords({firstWords : e.target[0].value,
-        secondWords: e.target[1].value})
-      }
+    firstWords: "",
+    secondWords: "",
+  });
+  function handleChange(e){
+    const {name, value} = e.target;
+    setMemeWords(prevMemeWords => ({
+      ...prevMemeWords,
+      [name]: value
+    }))
+    console.log(memeWords)
+  
+  }
+  function handleClick(e) {
+    e.preventDefault();
+   console.log(memeWords.firstWords,' ', memeWords.secondWords )
+  }
   return (
     <main className={MemeStyles.main}>
-    <form className={MemeStyles.form} onSubmit={handleClick}>
-      <input className={MemeStyles.input}type='text' placeholder='shut up'></input>
-      <input className={MemeStyles.input}type='text' placeholder='and take my money'></input>  
-      <button className={MemeStyles.button} type="submit" >Add Meme</button>  
-    </form>
-    <div className={MemeStyles.imageContainer}>
-      <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'/>
-      <h1 className={MemeStyles.firstWord}>{memeWords.firstWords}</h1>
-      <h1 className={MemeStyles.secondWord}>{memeWords.secondWords}</h1>
-    </div>
+      <form className={MemeStyles.form} onSubmit={handleClick}>
+        <input
+          className={MemeStyles.input}
+          type="text"
+          placeholder="shut up"
+          onChange={e=> handleChange(e) }
+          value={memeWords.firstWords}
+          name='firstWords'
+        ></input>
+        <input
+          className={MemeStyles.input}
+          type="text"
+          placeholder="and take my money"
+          onChange={e => handleChange(e)}
+          value={memeWords.secondWords}
+          name='secondWords'
+        ></input>
+        <button className={MemeStyles.button} type="submit">
+          Add Meme
+        </button>
+      </form>
+      <div className={MemeStyles.imageContainer}>
+        <img className={MemeStyles.image}src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+        <p className={MemeStyles.firstWord}>{memeWords.firstWords}</p>
+        <p className={MemeStyles.secondWord}>{memeWords.secondWords}</p>
+      </div>
     </main>
-  )
+  );
 }
