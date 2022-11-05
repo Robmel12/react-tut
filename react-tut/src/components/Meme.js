55a2c46ce551955351fb4f2266d9705db260cc35
Robmel12 <rob.melnyk@gmail.com>
2022-11-04 20:39:50 -0600

    Working on async fetch function for api, random pics

--- react-tut/src/components/Meme.js
+++ react-tut/src/components/Meme.js
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 import MemeStyles from "./Meme.module.scss";
 
 export default function Meme() {
+  let clientID = "5msstX_t_AjzFAx9GvGCYgxd43GMAzUspzP_5aH4IO4";
+  let endpoint = `https://api.unsplash.com/photos/?client_id=${clientID}`;
+  let data = [];
+  function async 
+  fetch(endpoint)
+    .then((res) => {
+      return res.json();
+    })
+    .then((jsonData) => {
+      data.push(jsonData);
+    });
+  const [photo, setphoto] = useState("second");
   const [memeWords, setMemeWords] = useState({
     firstWords: "",
     secondWords: "",
   });
-  function handleChange(e){
-    const {name, value} = e.target;
-    setMemeWords(prevMemeWords => ({
+  console.log(data[0]);
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setMemeWords((prevMemeWords) => ({
       ...prevMemeWords,
-      [name]: value
-    }))
-    console.log(memeWords)
-  
+      [name]: value,
+    }));
+    console.log(memeWords);
   }
   function handleClick(e) {
     e.preventDefault();
-   console.log(memeWords.firstWords,' ', memeWords.secondWords )
+    console.log(memeWords.firstWords, " ", memeWords.secondWords);
   }
   return (
     <main className={MemeStyles.main}>
@@ -26,24 +38,27 @@ export default function Meme() {
           className={MemeStyles.input}
           type="text"
           placeholder="shut up"
-          onChange={e=> handleChange(e) }
+          onChange={(e) => handleChange(e)}
           value={memeWords.firstWords}
-          name='firstWords'
+          name="firstWords"
         ></input>
         <input
           className={MemeStyles.input}
           type="text"
           placeholder="and take my money"
-          onChange={e => handleChange(e)}
+          onChange={(e) => handleChange(e)}
           value={memeWords.secondWords}
-          name='secondWords'
+          name="secondWords"
         ></input>
         <button className={MemeStyles.button} type="submit">
           Add Meme
         </button>
       </form>
       <div className={MemeStyles.imageContainer}>
-        <img className={MemeStyles.image}src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+        <img
+          className={MemeStyles.image}
+          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        />
         <p className={MemeStyles.firstWord}>{memeWords.firstWords}</p>
         <p className={MemeStyles.secondWord}>{memeWords.secondWords}</p>
       </div>
